d29a22737fe480b24a5e0338bd92e62e719cf22f
juro95 <79195394+juro95@users.noreply.github.com>
2023-05-06 12:08:28 +0200

    general concepts

--- general_concepts.py
+++ general_concepts.py
@@ -6,7 +6,19 @@ liste = [1, 2, 3, 4, 5]
 tupel = (1, 2, 3, 4, 5)
 dictionary = {"key1": "value1", "key2": "value2"}
 
+#Functions
+def scoping_function():
+    text
+    function_text = "Hello World!"
+    print("Hello World!")
+    return function_text
 
 
+#function_text cannot be accessed from outside the function as it is has local scope
+#print(function_text)
 
+#primitive data types example
+text = "Hello World!"
+number = 42
+boolean = True
 
