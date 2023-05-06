c4aea675bcfc2716c5e0256df2f83fc3fc393225
Jul <julklingel@Juls-MacBook-Pro.local>
2023-05-06 15:01:18 +0200

    poly

--- Object-oriented concepts/polymorphism.py
+++ Object-oriented concepts/polymorphism.py
@@ -2,8 +2,14 @@
 
 from abc import ABC, abstractmethod
 
+#common interface
+class IDescribable(ABC):
+    @abstractmethod
+    def describe(self) -> str:
+        pass
+
 # Base Class
-class Character:
+class Character(IDescribable):
     def __init__(self, name: str, level: int):
         self.name = name
         self.level = level
@@ -17,9 +23,6 @@ class Fighter(Character):
     def __init__(self, name: str, level: int, fighting_style: str):
         super().__init__(name, level)
         self.fighting_style = fighting_style
-
-    def describe(self) -> str:
-        return f"{self.name} is a level {self.level} Fighter with {self.fighting_style} fighting style."
 
 
 # Interface Inheritance
@@ -39,7 +42,7 @@ class Wizard(Character, ICastSpells):
 
 
 # Abstract Class
-class MagicalCreature(ABC):
+class MagicalCreature( IDescribable):
     @abstractmethod
     def magical_ability(self) -> str:
         pass
@@ -49,7 +52,12 @@ class Dragon(MagicalCreature):
     def magical_ability(self) -> str:
         return "The dragon breathes fire."
 
+    def describe(self) -> str:
+        return "The dragon is lvl 6."
+
+
 if __name__ == "__main__":
+    print('------------------')
     character = Character("Aragorn", 5)
     print(character.describe())
 
@@ -62,4 +70,14 @@ if __name__ == "__main__":
 
     dragon = Dragon()
     print(dragon.magical_ability())
+    print('------------------')
+
+    #Polymorphism can be illustrated by creating a list of objects of different classes that share a common method.
+    characters_and_creatures = [character, fighter, wizard, dragon]
+
+    for entity in characters_and_creatures:
+    
+        print(entity.describe())
+    print('------------------')
+
 
